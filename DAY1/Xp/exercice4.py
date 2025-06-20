be9c75de570ccbb8507b017b75ea0394055ea529
adn08Hiba <adnanihiba12345@gmail.com>
2025-06-20 17:27:57 +0200

    add Day1 and Day3

---
+++ DAY1/Xp/exercice4.py
@@ -0,0 +1,5 @@
+Height = float(input("tell me your height in cm:"))
+if Height > 145:
+    print("You are tall enough to ride.")
+else:
+    print("You are not tall enough you need to grow oythonsome more to ride.")
